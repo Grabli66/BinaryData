fe2b56eeee5c59f0c78a525cf7fb1a39968d4a8a
Егоров В.А <vegorov@sicon.ru>
2018-02-12 17:14:29 +0300

    Pool BinaryData

--- lib/binary_data_lib.dart
+++ lib/binary_data_lib.dart
@@ -4,4 +4,7 @@ import 'dart:collection';
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:object_pool/object_pool_lib.dart';
+
 part 'src/binary_data.dart';
+part 'src/pool_binary_data.dart';
