31a65e2aa1436c3b205e73fb6e667fab6ef5105c
Егоров В.А <vegorov@sicon.ru>
2018-02-19 17:19:58 +0300

    Get array

--- lib/binary_data_lib.dart
+++ lib/binary_data_lib.dart
@@ -3,7 +3,6 @@ library binary_data;
 import 'dart:collection';
 import 'dart:convert';
 import 'dart:typed_data';
-import 'dart:collection';
 
 part 'src/binary_data.dart';
 part 'src/binary_data_pooled.dart';
